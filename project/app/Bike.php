b65d7e1d2850758e8af2431e4203901bf0b8b219
Ajmal Hussain <ajmalhsn@hotmail.com>
2019-07-05 21:11:44 +0530

    first commit

---
+++ project/app/Bike.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Bike extends Model
+{
+    //
+    protected $fillable = [
+        'make',
+        'model',
+        'year',
+        'mods',
+        'picture'
+    ];
+}
---
+++ project/app/Http/Controllers/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+/**
+ * @OA\Info(title="My First API", version="0.1")
+ */
+
+/**
+ * @OA\Get(
+ *     path="/api/resource.json",
+ *     @OA\Response(response="200", description="An example resource")
+ * )
+ */
+}
